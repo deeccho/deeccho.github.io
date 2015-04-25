51163ef2d3d28b952bf52556a08833af54286c3a
deeccho <dccho@berkeley.edu>
2015-04-24 23:33:56 -0700

    finished most of blog view

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,10 +28,22 @@ $(document).ready(function() {
 	
 	$("#page-nav").change(function(){
 		if (document.getElementById("page-nav").value == 7){
-			$(this).next().nextAll().slideUp();
+			$(document.getElementsByClassName("month-posts")).slideUp();
 			$("#july-posts").slideDown();
+		} else if (document.getElementById("page-nav").value == 8){
+			$(document.getElementsByClassName("month-posts")).slideUp();
+			$("#august-posts").slideDown();
+		} else if (document.getElementById("page-nav").value == 9){
+			$(document.getElementsByClassName("month-posts")).slideUp();
+			$("#september-posts").slideDown();	
+		} else if (document.getElementById("page-nav").value == 10){
+			$(document.getElementsByClassName("month-posts")).slideUp();
+			$("#october-posts").slideDown();	
+		} else if (document.getElementById("page-nav").value == 11){
+			$(document.getElementsByClassName("month-posts")).slideUp();
+			$("#november-posts").slideDown();	
 		} else if (document.getElementById("page-nav").value == 12){
-			$(this).next().nextAll().slideUp();
+			$(document.getElementsByClassName("month-posts")).slideUp();
 			$("#december-posts").slideDown();
 		}
 	});
